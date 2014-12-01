47b1b5ff484ebc57737324ea5a852f9c32d6d444
Savannah Henderson <savannahjune@gmail.com>
2014-11-30 19:38:13 -0800

    share link styling

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def run_app():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
